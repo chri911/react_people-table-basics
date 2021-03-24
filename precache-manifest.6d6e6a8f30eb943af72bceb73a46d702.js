479fc728e016e918117e19842f3d29f6f73d70c4
chri911 <delightsome@bigmir.net>
2021-03-24 16:25:02 +0200

    Updates

---
+++ precache-manifest.6d6e6a8f30eb943af72bceb73a46d702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c660e3889f73cbd41183ca400fef9ec9",
+    "url": "/react_people-table-basics/index.html"
+  },
+  {
+    "revision": "859418aa7ff3200dba3c",
+    "url": "/react_people-table-basics/static/css/2.d1ab60d4.chunk.css"
+  },
+  {
+    "revision": "173a3827c6ff3b73a32b",
+    "url": "/react_people-table-basics/static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "859418aa7ff3200dba3c",
+    "url": "/react_people-table-basics/static/js/2.acb89bd6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_people-table-basics/static/js/2.acb89bd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "173a3827c6ff3b73a32b",
+    "url": "/react_people-table-basics/static/js/main.5b1ddbf8.chunk.js"
+  },
+  {
+    "revision": "30d22e43293fe0ee117b",
+    "url": "/react_people-table-basics/static/js/runtime-main.26bc3fa9.js"
+  }
+]);
